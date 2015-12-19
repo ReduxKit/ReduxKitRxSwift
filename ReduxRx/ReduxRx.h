e205848effcbf2ed6686e6e8c293e5dfe2a88388
Karl Bowden <karl@karlbowden.com>
2015-12-20 08:09:11 +1100

    Initial Commit

---
+++ ReduxRx/ReduxRx.h
@@ -0,0 +1,19 @@
+//
+//  ReduxRx.h
+//  ReduxRx
+//
+//  Created by Karl Bowden on 20/12/2015.
+//  Copyright © 2015 Redux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ReduxRx.
+FOUNDATION_EXPORT double ReduxRxVersionNumber;
+
+//! Project version string for ReduxRx.
+FOUNDATION_EXPORT const unsigned char ReduxRxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReduxRx/PublicHeader.h>
+
+
